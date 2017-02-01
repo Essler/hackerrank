1413f36094324af4b2a1754e3a6579669e7ef438
Brandon Essler <Essler@users.noreply.github.com>
2017-01-31 20:19:19 -0600

    Create submission.py

---
+++ contests/codeagon/challenges/back-to-origin/submission.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import sys
+
+xTreasure,yTreasure = input().strip().split(' ')
+xTreasure,yTreasure = [int(xTreasure),int(yTreasure)]
+n = int(input().strip())
+direction = []
+village = [xTreasure,yTreasure]
+
+for direction_i in range(n):
+    direction_t = [int(direction_temp) for direction_temp in input().strip().split(' ')]
+    direction.append(direction_t)
+    village[0] = village[0] - direction_t[0]
+    village[1] = village[1] - direction_t[1]
+    
+print(str(village[0]) + ' ' + str(village[1]))
